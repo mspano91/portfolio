d02090ac70ddeb22186c7286b5eacbf54ae46ef1
mspano <mspanopablo@gmail.com>
2023-11-22 16:11:12 -0300

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>este va ser mi portfolio JEJE</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
