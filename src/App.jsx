2e923494f7a43fad601f34285cdbb62c3cd8f651
mspano <mspanopablo@gmail.com>
2023-11-23 11:38:39 -0300

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import "./App.css";
+import { useState } from "react";
+import style from "../src/app.module.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+  console.log(darkMode);
+
   return (
     <>
-      <div className="App">
-        <h1>este va ser mi portfolio JEJE</h1>
+      <div className={`App ${darkMode ? "dark-mode" : ""}`}>
+        <div className={style.container}>
+          <h1>este va ser mi portfolio JEJE</h1>
+          {darkMode !== false ? (
+            <button onClick={handleDarkMode}>🌞</button>
+          ) : (
+            <button onClick={handleDarkMode}>🌚</button>
+          )}
+        </div>
       </div>
     </>
   );
