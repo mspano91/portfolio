11cf658766749b8a312afcc06cabffe061e635e8
mspano <mspanopablo@gmail.com>
2023-12-04 13:00:30 -0300

    responsive animation fixed

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -8,12 +8,18 @@ export default function AboutMe({ darkMode }) {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ["0 1", "1.33 1"],
+    offset: ["1 7", "0.8 1"],
   });
 
   return (
     <motion.div
-      style={{ scale: scrollYProgress, opacity: scrollYProgress }}
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 0.8, scale: scrollYProgress }}
+      style={{
+        scale: scrollYProgress,
+        opacity: scrollYProgress,
+        x: window.innerWidth > 0 ? 0 : 0,
+      }}
       ref={ref}
       className={style.conainerAbout}
     >
--- src/Tittle/Picture.jsx
+++ src/Tittle/Picture.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import image from "../assets/img/mati8.png";
 import style from "./tittle.module.css";
+import { useRef } from "react";
+import { useScroll, motion } from "framer-motion";
 
 export default function Picture() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  });
+
   return (
-    <div className={style.ContainerPicture}>
+    <motion.div
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 0.8, scale: scrollYProgress }}
+      style={{
+        scale: scrollYProgress,
+        opacity: scrollYProgress,
+        x: window.innerWidth > 0 ? 0 : 0,
+      }}
+      ref={ref}
+      className={style.ContainerPicture}
+    >
       <img className={style.picture} src={image} alt="" />
-    </div>
+    </motion.div>
   );
 }
