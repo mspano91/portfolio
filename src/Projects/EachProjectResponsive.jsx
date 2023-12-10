400eb1b072dfc67e807c6a73779db77b15a04a15
mspano <mspanopablo@gmail.com>
2023-12-10 15:52:15 -0300

    buttom plus ok

--- src/Projects/EachProjectResponsive.jsx
+++ src/Projects/EachProjectResponsive.jsx
@@ -1,26 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "../Projects/EachProjectResponsive.module.css";
 import videos from "../Projects/videos.js";
 
 export default function EachProjectResponsive() {
+  const itemsPerPage = 1;
+  const [visibleVideos, setVisibleVideos] = useState(
+    videos.slice(0, itemsPerPage)
+  );
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleLoadMore = () => {
+    const nextPage = currentPage + 1;
+    const startIndex = (nextPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const newVisibleVideos = videos.slice(0, endIndex);
+
+    setVisibleVideos(newVisibleVideos);
+    setCurrentPage(nextPage);
+  };
   return (
     <div>
       <div className={style.carousel}>
         <div className={style.innerCarousel}>
-          {videos.map((video, index) => (
+          {visibleVideos.map((video, index) => (
             <div className={style.item}>
               <img className={style.picture} src={video.imagen} alt="" />
               <div className={style.description}>
                 <h2>{video.tittle}</h2>
                 <p>{video.description}</p>
                 <div className={style.links}>
-                  <span className={style.box}>Github</span>
-                  <span className={style.box}>Deploy</span>
+                  {video.git && (
+                    <a
+                      className={style.box}
+                      href={video.git}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Github
+                    </a>
+                  )}
+
+                  {video.git2 && (
+                    <a
+                      className={style.box}
+                      href={video.git2}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Git Front
+                    </a>
+                  )}
+
+                  {video.git1 && (
+                    <a
+                      className={style.box}
+                      href={video.git1}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Git Back
+                    </a>
+                  )}
+
+                  {video.deploy && (
+                    <a
+                      className={style.box}
+                      href={video.deploy}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Deploy
+                    </a>
+                  )}
                 </div>
               </div>
             </div>
           ))}
         </div>
+      </div>
+      <div className={style.loadMore}>
+        <button className={style.moreBtn} onClick={handleLoadMore}>
+          +
+        </button>
       </div>
     </div>
   );
