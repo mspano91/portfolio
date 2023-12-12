4c8592ed786af22b98d7035732ed3c2d315c0e9e
mspano <mspanopablo@gmail.com>
2023-12-11 23:01:27 -0300

    now2

--- src/Tittle/CV.jsx
+++ src/Tittle/CV.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import style from "./Cv.module.css";
-import cv from "./cv/spanoCv.pdf";
+import cv1 from "./cv/spanoCv.pdf";
 
 export default function CV() {
   return (
     <div>
-      <a className={style.Btn} download href={cv}>
+      <a className={style.Btn} download href={cv1}>
         <svg
           className={style.svgIcon}
           viewBox="0 0 384 512"
